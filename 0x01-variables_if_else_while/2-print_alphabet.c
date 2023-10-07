0a82e442fdf5f913919b6f4623d7ae412fbc4621
hawi-claire <7hawi7@gmail.com>
2023-10-07 02:16:21 -0700

    Variables, if, else, while) exit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,16 @@
-#include <stdlio.h>
+#include <stdio.h>
 /**
-  *main - Entry point
-  *Description: 'Print alphabets loercase
-  *Return: Always 0
-  */
+ *main - Entry point
+ *Description: 'Print alphabets loercase
+ *Return: Always 0
+ */
 int main(void)
 {
-	int n =97;
-
-	while (n < = 122)
+	for (char c = 'a'; c <= 'z'; c++)
 	{
-		putchar(n);
-		n++;
+		putchar(c);
 	}
 	putchar('\n');
+
 	return (0);
 }
