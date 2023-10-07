8b769c420338825db279f6356e25e59d3e8dd698
hawi-claire <7hawi7@gmail.com>
2023-10-07 06:28:12 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print comb of numbers'
+ * Return: always 0
+ */
+int main(void)
+{
+	int m:
+		for (n = 48; m <= 57; n++)
+		{
+			putchar(n);
+			if (n == 57)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+		}
+	putchar('\n');
+	return (0);
+}
