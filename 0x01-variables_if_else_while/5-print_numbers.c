529b50f12ebf17f2317ddc2e6dd744c8dec29a10
hawi-claire <7hawi7@gmail.com>
2023-10-07 16:27:24 -0700

    Variables, if, else,while

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: 'print single digit numbers of base ten'
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 0; n++)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
