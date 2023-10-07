25249dd47b03b0572dc28bf3c8675e11bf8ef1fd
hawi-claire <7hawi7@gmail.com>
2023-10-06 17:55:55 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdlio.h>
+/**
+  *main - Entry point
+  *Description: 'Print alphabets loercase
+  *Return: Always 0
+  */
+int main(void)
+{
+	int n =97;
+
+	while (n < = 122)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
