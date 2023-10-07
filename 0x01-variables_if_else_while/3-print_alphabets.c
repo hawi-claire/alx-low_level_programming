c7d776e035983197ec2b6253ce2b3effb157a1d2
hawi-claire <7hawi7@gmail.com>
2023-10-07 15:15:18 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 /**
- * main - Entry point
- * Return: Always 0 (Success)
- */
-
+  *main = Entry point
+  *Description: 'Print uppercase and lowercase of alphabet'
+  *Return: always 0
+  */
 int main(void)
 {
-	char c = 'a'
-		while(c <='z' || c <= 'z')
-		{
-			putchar(c);
-			c++;
-			if(c> 'z')
-		}
+	int n = 97;
+	int m = 65;
 
-
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (n <= 98)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
 }
