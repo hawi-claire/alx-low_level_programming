1ce5b4437b2c86149acdf2e0c39328cdef2d479e
hawi-claire <7hawi7@gmail.com>
2023-10-07 04:08:42 -0700

    Variables, if, else, while

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c = 'a'
+		while(c <='z' || c <= 'z')
+		{
+			putchar(c);
+			c++;
+			if(c> 'z')
+		}
+
+
+}
