e0abf413d29f8395a99e0fb84b15428f6063f51c
hawi-claire <7hawi7@gmail.com>
2023-10-07 15:25:44 -0700

    Variables, if, else, while

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-  *main = Entry point
-  *Description: 'Print uppercase and lowercase of alphabet'
-  *Return: always 0
-  */
+ * main - Entry point
+ * Description: 'print alphabets lowercase and uppercase'
+ * Return: always 0
+ */
 int main(void)
 {
 	int n = 97;
