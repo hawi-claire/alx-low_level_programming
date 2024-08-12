2821a374caa33b1b2a035c78db1ebfc1bb6505aa
hawi-claire <7hawi7@gmail.com>
2024-08-12 10:11:21 -0700

    [Fix] File descriptor name

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,21 +1,23 @@
 #include "main.h"
+
 /**
  * create_file - program creates a file
  * @filename: filename
  * @text_content: content in the file
  * Return: 1 if success , -1 if it fails
  */
+
 int create_file(const char *filename, char *text_content)
 {
-	int files_d;
+	int f_descriptor;
 	int nletters;
 	int rwr;
 
 	if (!filename)
 		return (-1);
-	files_d = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (files_d == -1)
+	f_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+	if (f_descriptor == -1)
 		return (-1);
 
 	if (!text_content)
@@ -23,12 +25,12 @@ int create_file(const char *filename, char *text_content)
 	for (nletters = 0; text_content[nletters]; nletters++)
 		;
 
-	rwr = write(files_d, text_content, nletters);
+	rwr = write(f_descriptor, text_content, nletters);
 
 	if (rwr == -1)
 		return (-1);
 
-	close(files_d);
+	close(f_descriptor);
 
 	return (-1);
 }
