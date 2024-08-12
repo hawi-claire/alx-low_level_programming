e6304ff9c928e1be9dcefde5adb5595328718522
hawi-claire <7hawi7@gmail.com>
2024-08-12 06:50:22 -0700

    [Fix] Undeclared errors and add header files

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,6 +2,8 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <fcntl.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 /**
  * read_textfile - This reads a text file and prints it
@@ -15,11 +17,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int f_descriptor;
 	char *buff;
+	ssize_t lenr, lenw;
 
 	if (filename == NULL)
 		return (0);
 
-	f_descriptor = open(filename, 0_RDONLY);
+	f_descriptor = open(filename, O_RDONLY);
 	if (f_descriptor == -1)
 		return (0);
 
@@ -29,14 +32,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(f_descriptor);
 		return (0);
 	}
-	lenr = read(files_d, buff, letters);
-	close(files_d);
+	lenr = read(f_descriptor, buff, letters);
+	close(f_descriptor);
 	if (lenr == -1)
 	{
 		free(buff);
 		return (0);
 	}
-	lenm = write(STOUT_FILENO, buff, lenr);
+	lenw = write(STDOUT_FILENO, buff, lenr);
 	free(buff);
 	if (lenr != lenw)
 		return (0);
