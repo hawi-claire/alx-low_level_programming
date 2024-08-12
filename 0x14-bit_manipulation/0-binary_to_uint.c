209d92e2a4e349d3680cd70287c7f9c69fc61da7
hawi-claire <7hawi7@gmail.com>
2024-08-12 15:23:07 -0700

    [Fix] Code

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,3 +9,18 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int result = 0;
+	int len = 0;
+
+	if (b[len] == '\0')
+		return (0);
+
+	while ((b[len] == '0') || (b[len] == '1'))
+	{
+		result <<=1;
+		result += b[len] - '0';
+		len++;
+	}
+
+	return (result);
+}
