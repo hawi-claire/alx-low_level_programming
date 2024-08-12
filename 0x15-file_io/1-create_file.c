601a2463d639ec17162a2d91822c4edac600b00d
hawi-claire <7hawi7@gmail.com>
2024-08-12 13:37:12 -0700

    [Fix] Code, variables

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,36 +1,33 @@
 #include "main.h"
 
 /**
- * create_file - program creates a file
- * @filename: filename
- * @text_content: content in the file
- * Return: 1 if success , -1 if it fails
+ * create_file - Creates a file.
+ * @filename: A pointer to the name of the file to create.
+ * @text_content: A pointer to a string to write to the file.
+ *
+ * Return: If the function fails - -1.
+ *         Otherwise - 1.
  */
-
 int create_file(const char *filename, char *text_content)
 {
-	int f_descriptor;
-	int nletters;
-	int rwr;
+	int f_open, f_write, len = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	f_descriptor = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-	if (f_descriptor == -1)
+	if (text_content != NULL)
+	{
+		for (len = 0; text_content[len];)
+			len++;
+	}
+
+	f_open = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	f_write = write(o, text_content, len);
+
+	if (f_open == -1 || f_write == -1)
 		return (-1);
 
-	if (!text_content)
-		text_content = "";
-	for (nletters = 0; text_content[nletters]; nletters++)
-		;
+	close(f_open);
 
-	rwr = write(f_descriptor, text_content, nletters);
-
-	if (rwr == -1)
-		return (-1);
-
-	close(f_descriptor);
-
-	return (-1);
+	return (1);
 }
